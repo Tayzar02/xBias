99041d53fd5881439fb96a6de073873841a90c49
Tayzar02 <tjhouse0508@gmail.com>
2021-10-07 22:24:12 -0400

    Implemented compareBias

--- src/main/java/com/xbias/Scraper.java
+++ src/main/java/com/xbias/Scraper.java
@@ -18,14 +18,14 @@ class Scraper
 //Downloads the webpage so we're not hitting the page with a bunch of traffic (cuz that's mean)
     Document doc = DownloadWebPage(html);
 //Converts the file to a string so we can parse it through compareBias	
-		Element table = doc.getElementById(id);  //going to have to change this Id site to site, not everybody calls their id for the main body of the page wrapper....
-		Elements rows = table.getElementsByTag("div");
-    String parsedFile = "null";
+		Element table = doc.getElementById(id);
+		Elements rows = table.getAllElements(); //temporary solution, going to have to get smarter with tags, currently collects ALL elements on a page 
+    String parsedFile = "";
 		for (Element row : rows) {
-			Elements tds = row.getElementsByTag("p");
+			Elements tds = row.getAllElements();
 			for (int i = 0; i < tds.size(); i++) 
       { 
-        parsedFile += (tds.get(i).text());
+        parsedFile = (rows.get(i).text());
 			}
     }
     //System.out.println("Parsed File: " + parsedFile); //DEBUG: Prints parsed file
---
+++ xBiasML.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
